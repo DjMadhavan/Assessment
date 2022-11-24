fe4921bb331b9dcf6b83a3f225978f0b53bfa88d
DjMadhavan <118958197+DjMadhavan@users.noreply.github.com>
2022-11-24 12:00:41 +0530

    Add files via upload

---
+++ 23-11-2022/Code/Java.java
@@ -0,0 +1,11 @@
+class Java 
+{
+	public static void main(String []args)
+	{
+		System.out.println("Hello everyone My self Madhavan");
+		System.out.println("I am from Arakkonam");
+		System.out.println("I am Completed BCA");
+		System.out.println("Thats all , Thank you");
+	}	
+
+}
