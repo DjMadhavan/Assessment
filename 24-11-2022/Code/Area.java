1c9d548a79c2f563eb515cda9c48ce6b1b8661c9
DjMadhavan <118958197+DjMadhavan@users.noreply.github.com>
2022-11-24 14:27:20 +0530

    Add files via upload

---
+++ 24-11-2022/Code/Area.java
@@ -0,0 +1,14 @@
+class Area
+{
+   public static void main(String []args)
+	{
+		System.out.println("Basic");
+		System.out.println("10+20");
+		System.out.println(10+20);
+		System.out.println(10+20+"output :");
+		System.out.println(10+20+"output :"+10+20);
+		System.out.println(10+20+"output :"+10+20*30);
+		System.out.println(10+20+"output :"+(10+90)+23);
+		System.out.println("out"+"put");
+	}
+}
