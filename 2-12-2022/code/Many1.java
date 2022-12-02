f66ba5e8474a7cd941885c7821acf10923a8fb00
DjMadhavan <118958197+DjMadhavan@users.noreply.github.com>
2022-12-02 13:45:30 +0530

    Add files via upload

---
+++ 2-12-2022/code/Many1.java
@@ -0,0 +1,20 @@
+class Many1
+{
+     public static void main(String []args)
+	{
+     int i=10;
+	 int j=2;
+	 i += 12;
+			System.out.println(i+j);
+		System.out.println(i-j);
+		System.out.println(i*j);
+		System.out.println(i/j);
+		System.out.println(i%j);
+		System.out.println(j==2);
+		System.out.println(j!=3);
+		System.out.println((100>1) && (200>350) && (1<2));
+		System.out.println((100>1) || (200>350) || (1<2));
+    }
+
+
+}
